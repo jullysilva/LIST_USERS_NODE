e0977643e1f7be367dc79231565baa42dcd4b391
Jully Silva <jullyksilva@hotmail.com>
2024-06-28 13:08:29 -0300

    [CONFIG]: Update config publication in github actions

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 let usersList = [];
