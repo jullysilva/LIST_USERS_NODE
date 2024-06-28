41e7f5b36cb63a570d4776f19d23c7f410d9ef6a
Jully Silva <jullyksilva@hotmail.com>
2024-06-28 12:56:12 -0300

    [CONFIG]: Configuration deploy in github pages

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { readFileSync, writeFileSync } = require("fs");
 const bodyParser = require("body-parser");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
