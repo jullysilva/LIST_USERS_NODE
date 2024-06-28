63f6f95f0bf324d5e8480363dac01112bb791a58
Jully Silva <jullyksilva@hotmail.com>
2024-06-28 13:32:17 -0300

    [CONFIG]: Update config deploy

--- build.js
+++ build.js
@@ -7,14 +7,25 @@ const outputDir = path.join(__dirname, "public");
 
 async function renderEJS() {
   try {
+    // Limpar a pasta de saída
     await fs.emptyDir(outputDir);
 
+    // Definir dados padrão para renderizar as views
+    const data = {
+      users: [], // ou fornecer dados mockados
+      currentPage: 1,
+      totalPages: 1,
+      searchQuery: "",
+    };
+
+    // Listar todos os arquivos EJS no diretório de views
     const files = await fs.readdir(viewsDir);
 
+    // Renderizar cada arquivo EJS e salvar como HTML
     for (const file of files) {
       if (path.extname(file) === ".ejs") {
         const filePath = path.join(viewsDir, file);
-        const html = await ejs.renderFile(filePath, {});
+        const html = await ejs.renderFile(filePath, data);
         const outputFilePath = path.join(
           outputDir,
           path.basename(file, ".ejs") + ".html"
